417c33083135c4600b235fc530ce48b05883062c
mike-droid <miguelreyesmoreno@hotmail.com>
2023-07-16 16:09:55 -0600

    Clase 7 - useFactory en nest@v7

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,10 +2,14 @@ import { Injectable, Inject } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  constructor(@Inject('API_KEY') private apiKey: string) {}
+  constructor(
+    @Inject('API_KEY') private apiKey: string,
+    @Inject('TASKS') private tasks: any[],
+  ) {}
   //* Ahora este valor inyectado se puede usar en toda la app
 
   getHello(): string {
+    console.log(this.tasks);
     return `Hello World! -> ${this.apiKey}`;
   }
 }
