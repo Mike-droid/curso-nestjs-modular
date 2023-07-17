125bb30a5d8cc26fce01f238477c6421d16f9e39
mike-droid <miguelreyesmoreno@hotmail.com>
2023-07-16 21:18:15 -0600

    Clase 10 - configuracion por ambientes

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,18 @@
 import { Injectable, Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
   constructor(
     @Inject('API_KEY') private apiKey: string,
     @Inject('TASKS') private tasks: any[],
+    private configService: ConfigService,
   ) {}
   //* Ahora este valor inyectado se puede usar en toda la app
 
   getHello(): string {
-    console.log(this.tasks);
-    return `Hello World! -> ${this.apiKey}`;
+    const apiKey = this.configService.get('MY_API_KEY');
+    const dbName = this.configService.get('DATABASE_NAME');
+    return `Hello World! -> ${apiKey} and DB name -> ${dbName}`;
   }
 }
