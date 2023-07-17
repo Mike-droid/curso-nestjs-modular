6b0ba08eade9595d334ac57072f0c6705204637e
mike-droid <miguelreyesmoreno@hotmail.com>
2023-07-17 13:54:44 -0600

    Clase 14 - extendiendo la documentacion

--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -9,21 +9,21 @@ import {
   Delete,
   HttpStatus,
   HttpCode,
-  Res,
-  // ParseIntPipe,
 } from '@nestjs/common';
 
-import { Response } from 'express';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { ParseIntPipe } from '../../common/parse-int.pipe';
 import { CreateProductDto, UpdateProductDto } from '../dtos/products.dto';
 
 import { ProductsService } from './../services/products.service';
 
+@ApiTags('products')
 @Controller('products')
 export class ProductsController {
   constructor(private productsService: ProductsService) {}
 
   @Get()
+  @ApiOperation({ summary: 'List of products' })
   getProducts(
     @Query('limit') limit = 100,
     @Query('offset') offset = 0,
