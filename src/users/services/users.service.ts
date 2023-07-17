c6f61646010199d5801c31b518397c81f996bab1
mike-droid <miguelreyesmoreno@hotmail.com>
2023-07-16 20:11:43 -0600

    Clase 9 - modulo de configuracion

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,5 @@
-import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 import { User } from '../entities/user.entity';
 import { CreateUserDto, UpdateUserDto } from '../dtos/user.dto';
@@ -8,7 +9,7 @@ import { ProductsService } from '../../products/services/products.service';
 export class UsersService {
   constructor(
     private productsService: ProductsService,
-    @Inject('API_KEY') private apiKey: string,
+    private configService: ConfigService,
   ) {}
 
   private counterId = 1;
@@ -22,6 +23,9 @@ export class UsersService {
   ];
 
   findAll() {
+    const apiKey = this.configService.get('API_KEY');
+    const dbName = this.configService.get('DATABASE_NAME');
+    console.log(`api key -> ${apiKey} | db name -> ${dbName}`);
     return this.users;
   }
 
